97390ec4b02ad5cecab42a25d64fddca247cc4cf
cokerserkan09 <cokerserkan@gmail.com>
2026-08-30 19:17:34 +0300

    v3.9 kayit baslangic eklemesini fonksiyon basina al

--- .github/scripts/v39_self_fix.py
+++ .github/scripts/v39_self_fix.py
@@ -7,23 +7,19 @@ end=s.find('# Finalize aninda AAC sesini video ile mux et.', start)
 if start < 0 or end < 0:
     raise SystemExit('v39 bolumu bulunamadi')
 new='''# Kayit baslamadan once gercek playback capture servisini baslat.
-startpos=s.find('    private fun startRecording() {')
-endpos=s.find('    private fun ', startpos+20)
-if startpos < 0 or endpos < 0: raise SystemExit('startRecording block bulunamadi')
-rb=s[startpos:endpos]
-anchor='        val recording = pendingRecording\\n'
-if anchor not in rb: raise SystemExit('pendingRecording anchor bulunamadi')
-insert=''' + "'''" + '''        if ((selectedVideoAudio == "playback" || selectedVideoAudio == "mixed") && !startPlaybackCaptureIfNeeded()) {
+marker='    private fun startRecording() {\\n'
+if marker not in s: raise SystemExit('startRecording bulunamadi')
+insert=''' + "'''" + '''    private fun startRecording() {
+        val realPlaybackMode = videoAudioSource()
+        if ((realPlaybackMode == "playback" || realPlaybackMode == "mixed") && !startPlaybackCaptureIfNeeded()) {
             toast("Medya sesi yakalama izni gerekli.")
             requestPlaybackCapturePermission()
             return
         }
-        val recording = pendingRecording
 ''' + "'''" + '''
-rb=rb.replace(anchor, insert, 1)
-s=s[:startpos]+rb+s[endpos:]
+s=s.replace(marker, insert, 1)
 
 '''
 s=s[:start]+new+s[end:]
 p.write_text(s,encoding='utf-8')
-print('v3.9 anchor duzeltildi')
+print('v3.9 kayit baslangic eklemesi duzeltildi')
